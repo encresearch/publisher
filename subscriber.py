5472091799e9cad22fd01ce6081645588019a0bd
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-06-12 15:58:55 -0400

    added functionality to receive csv file and rewrite it in a new file

--- subscriber.py
+++ subscriber.py
@@ -2,14 +2,16 @@ import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-        print("Connected with result code {}".format(rc))
+    print("Connected with result code {}".format(rc))
 # Subscribing in on_connect() means that if we lose the connection and
 # reconnect then subscriptions will be renewed.
-        client.subscribe("RasPi1/10Hz")
+    client.subscribe("RasPi1/10Hz")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-        print(msg.payload)
+    print("received message")
+    with open('random.csv', 'wb') as fd:
+        fd.write(msg.payload)
 
 client = mqtt.Client()
 client.on_connect = on_connect
