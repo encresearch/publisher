6d69a477a438f1c546e03a0b3358c48fd95e94a8
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-06-19 12:52:39 -0400

    Added Travis file and decoupled main code

--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -11,15 +11,20 @@ import pandas as pd
 import numpy as np
 import time
 
-broker = "35.237.36.219" # static IP of mosquitto broker
-port = 1883
-GAIN = 1 # We are going to use same gain for all of them
-
 # create four ADS115 instances 
 adc0 = Adafruit_ADS1x15.ADS1115(0x48) # ADR to GRN
 adc1 = Adafruit_ADS1x15.ADS1115(0x49) # ADR to VDD
 adc2 = Adafruit_ADS1x15.ADS1115(0x4A) # ADR to SDA
 adc3 = Adafruit_ADS1x15.ADS1115(0x4B) # ADR to SCL
+
+def connect_to_broker(client_id, host, port, keepalive, on_connect, on_publish):
+    # Params -> Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
+    # We set clean_session False, so in case connection is lost, it'll reconnect with same ID
+    client = mqtt.Client(client_id=client_id, clean_session=False)
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    connection = client.connect(host, port, keepalive)
+    return (client, connection)
 
 def read_ten_hz():
     """ 
@@ -27,14 +32,25 @@ def read_ten_hz():
     creates a numpy array which is then converted to a panda's dataframe and into a CSV file
     and sent to the MQTT broker with topic RasPi1/10Hz
     """
+    client_id = "TEN_HZ"
+    host = "35.237.36.219" # static IP of mosquitto broker
+    port = 1883
+    keepalive = 30
+    GAIN = 1 # We are going to use same gain for all of them
+    headers = ['adc', 'channel', 'time_stamp', 'value'] # Headers of the upcoming csv file
+
+    def on_connect(client, userdata, flags, rc):
+        pass
+
     def on_publish(client, userdata, result):
         # Function for clients1's specific callback when pubslishing message
         print("Data Published")
         pass
-    headers = ['adc', 'channel', 'time_stamp', 'value'] # Headers of the upcoming csv file
-    client1 = mqtt.Client("ten_hz") #create an mqtt client instance
-    client1.on_publish = on_publish #assign function callback
-    client1.connect(broker, port) #establish connection to the Mosquitto broker
+      
+    client, connection = connect_to_broker(client_id=client_id, host=host, port=port, keepalive=keepalive, on_connect=on_connect, on_publish=on_message)
+    
+    client.loop_start()
+
     while True:
         values = np.empty((0, 4)) #create an empty array with 4 'columns'
         for _ in range(600): # The following should be repeated 600 times to complete a minute
@@ -54,9 +70,10 @@ def read_ten_hz():
         dataframe.to_csv('ten_hz.csv', columns=headers, index=False)
         f = open('ten_hz.csv')
         csv = f.read()
-        client1.loop_start()
-        client1.publish("RasPi1/10Hz", csv, 2)
-        client1.loop_stop()
+        client.publish("RasPi1/10Hz", csv, 2)
+
+def main():
+    read_ten_hz()
 
 if __name__ == '__main__':
-    read_ten_hz()
+    main()
