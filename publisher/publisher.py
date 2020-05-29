b5dbad0f8a62fb3b060db665d124b3c060094073
Seb <seb.arboleda@outlook.com>
2020-05-28 23:02:49 -0400

    Read only from two ADCs (ADR->GRD & ADR->VDD)

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -24,7 +24,6 @@ All readings are done at 10Hz.
 
 TODO [Magnetometer functions coming up]
 """
-
 import logging
 import time
 import os
@@ -34,25 +33,32 @@ import paho.mqtt.client as mqtt
 import pandas as pd
 import numpy as np
 
-HOST = os.getenv("BROKER_IP", "mqtt.eclipse.org") # static IP of mosquitto broker
+# TOPIC: <country>/<city>/<device_num>/<reading_type>
+# For development purposes we are using Eclipse's public mosquitto broker
+# static IP.
+HOST = os.getenv("BROKER_IP", "mqtt.eclipse.org")
 PORT = os.getenv("BROKER_PORT", 1883)
 KEEPALIVE = 30
-TOPIC = os.getenv("TOPIC", "usa/quincy/1") # defaults to $("usa/quincy/1")
+TOPIC = os.getenv("TOPIC", "usa/quincy/1")
 GAIN = 1
-HEADERS = ['adc', 'channel', 'time_stamp', 'value'] # Headers of the upcoming csv file
-client_id = "{0}".format("/TEN_HZ") # <country>/<city>/<device_num>/<reading_type>
+DF_HEADERS = ['adc', 'channel', 'time_stamp', 'value']
+client_id = "{0}".format("/TEN_HZ")
 data_rate = 475
 
-# create four ADS115 instances with different addresses
-# based on the connection of the ADR (address) pin
+# Create four ADS115 instances with different addresses
+# based on the connection of the ADR (address) pin:
+#   - 0x48 (1001000) ADR -> GND
+#   - 0x49 (1001001) ADR -> VDD
+#   - 0x4A (1001010) ADR -> SDA
+#   - 0x4B (1001011) ADR -> SCL
 # Data Rate samples are chosen based on the frequency we want to pull data
 # from it. data_rate indicates the time it will take in measuring the analog data
-# TODO do this in hex with an iteration while storing in a list(adcs).
+# We have capacity for 4 ADC's, but for now only two are needed. To add the
+# other two, just uncomment the following lines:
+# adc2 = Adafruit_ADS1x15.ADS1115(address=0x4A)
+# adc3 = Adafruit_ADS1x15.ADS1115(address=0x4B)
 adc0 = Adafruit_ADS1x15.ADS1115(address=0x48)
 adc1 = Adafruit_ADS1x15.ADS1115(address=0x49)
-adc2 = Adafruit_ADS1x15.ADS1115(address=0x4A)
-adc3 = Adafruit_ADS1x15.ADS1115(address=0x4B)
-adcs = [adc0, adc1, adc2, adc3]
 
 def connect_to_broker(client_id, host, port, keepalive, on_connect, on_publish):
     """
@@ -68,49 +74,43 @@ def connect_to_broker(client_id, host, port, keepalive, on_connect, on_publish):
     return (client, connection)
 
 def get_readings():
+    """
+    Returns an array of 4 columns with headers -> HEADERS and the ADCs 10Hz
+    measurements for each pin, for 1 minute (2,400 readings per ADC).
+
+    | adc | channel | time_stamp |  value  |
+    |-----|---------|------------|---------|
+    |  0  |    0    |   xxxxxx   |   YYYY  |
+    |-----|---------|------------|---------|
+    | ... |   ...   |    ...     |   ...   |
+    |-----|---------|------------|---------|
+    """
     values = np.empty((0, 4)) #create an empty array with 4 'columns'
-    # TODO checkk out how we do it in testing and do the same iteration
+    # TODO check out how we do it in testing and do the same iteration
     # The following should be repeated 600 times to complete a minute
     for _ in range(600):
         # Time measurement to know how long this procedure takes
         now = time.time()
-        values = np.vstack((values, np.array([1, 1, datetime.now(), adc0.read_adc(0, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([1, 2, datetime.now(), adc0.read_adc(1, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([1, 3, datetime.now(), adc0.read_adc(2, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([1, 4, datetime.now(), adc0.read_adc(3, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([2, 1, datetime.now(), adc1.read_adc(0, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([2, 2, datetime.now(), adc1.read_adc(1, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([2, 3, datetime.now(), adc1.read_adc(2, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([2, 4, datetime.now(), adc1.read_adc(3, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([3, 1, datetime.now(), adc2.read_adc(0, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([3, 2, datetime.now(), adc2.read_adc(1, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([3, 3, datetime.now(), adc2.read_adc(2, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([3, 4, datetime.now(), adc2.read_adc(3, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([4, 1, datetime.now(), adc3.read_adc(0, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([4, 2, datetime.now(), adc3.read_adc(1, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([4, 3, datetime.now(), adc3.read_adc(2, gain=GAIN, data_rate=data_rate)])))
-        values = np.vstack((values, np.array([4, 4, datetime.now(), adc3.read_adc(3, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([0, 0, datetime.now(), adc0.read_adc(0, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([0, 1, datetime.now(), adc0.read_adc(1, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([0, 2, datetime.now(), adc0.read_adc(2, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([0, 3, datetime.now(), adc0.read_adc(3, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([1, 0, datetime.now(), adc1.read_adc(0, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([1, 1, datetime.now(), adc1.read_adc(1, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([1, 2, datetime.now(), adc1.read_adc(2, gain=GAIN, data_rate=data_rate)])))
+        values = np.vstack((values, np.array([1, 3, datetime.now(), adc1.read_adc(3, gain=GAIN, data_rate=data_rate)])))
         operation_time = time.time()-now
         if operation_time < 0.1:
             time.sleep(0.1 - operation_time)
-    dataframe = pd.DataFrame(values, columns=HEADERS)
+    dataframe = pd.DataFrame(values, columns=DF_HEADERS)
     return dataframe
 
-<<<<<<< HEAD
-def send_readings(dataframe, client=None):
-    dataframe.to_csv('ten_hz.csv', columns=HEADERS, index=False)
-    f = open('ten_hz.csv')
-    csv = f.read()
-    if client is not None:
-        client.publish(TOPIC, csv, 2)
-=======
 def send_readings(dataframe, client):
     """Recieves a Pandas' dataframe, creates CSV file and sends through mqtt."""
-    dataframe.to_csv('ten_hz.csv', columns=HEADERS, index=False)
+    dataframe.to_csv('ten_hz.csv', columns=DF_HEADERS, index=False)
     readings_file = open('ten_hz.csv')
     csv = readings_file.read()
     client.publish(TOPIC, csv, 2)
->>>>>>> 826469c... Add logging for general exceptions
 
 def main():
     """
@@ -143,11 +143,6 @@ def main():
         send_readings(dataframe, client) # Then it is sent to the broker client
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    main()
-    # TODO capture exceptions
-    # try: main(); except Exception as e: send_report(); start_over();
-=======
     try:
         main()
     except Exception as e:
@@ -155,4 +150,3 @@ if __name__ == '__main__':
         date = datetime.now().strftime('%d %b %Y %H:%M')
         logging.basicConfig(filename='publisher.log')
         logging.exception("%s %s", msg, date)
->>>>>>> 826469c... Add logging for general exceptions
