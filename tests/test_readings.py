a56d71bf57fa218cea6e3c5a13aac81b7ea8b9cb
Seb <sebasarboleda22@gmail.com>
2020-06-24 11:49:51 -0400

    Fix outdated tests

--- tests/test_readings.py
+++ tests/test_readings.py
@@ -1,8 +1,8 @@
 """File with most reading tests"""
 import pytest
 
-#TODO def test_connect_to_broker():
-"""	from publisher.publisher import (
+def test_connect_to_broker():
+    from publisher.publisher import (
         HOST, PORT, KEEPALIVE, TOPIC, client_id, connect_to_broker
     )
     def on_connect(client, userdata, flags, rc):
@@ -18,8 +18,8 @@ import pytest
         on_connect=on_connect,
         on_publish=on_publish
     )
+    import pdb; pdb.set_trace()
     # Now, lets test a fake connection
-"""
 
 @pytest.fixture
 def readings_df():
@@ -31,13 +31,12 @@ def test_expected_readings():
     """Outputs readings from all pins for calibration purposes."""
     from publisher.publisher import (
         Adafruit_ADS1x15,
-        GAIN, data_rate,
+        GAIN,
+	data_rate,
         adc0,
-        adc1,
-        adc2,
-        adc3
+        adc1
     )
-    adcs = [(0, adc0), (1, adc1), (2, adc2), (3, adc3)]
+    adcs = [(0, adc0), (1, adc1)]
     readings = []
     ansi_code_yellow_init = "\033[93m"
     ansi_code_yellow_end = "\033[00m"
@@ -46,7 +45,7 @@ def test_expected_readings():
             ansi_code_yellow_init,
             adc[0],
             ansi_code_yellow_end
-        )), 
+        )),
         for pin in range(4):
             reading = adc[1].read_adc(pin, gain=GAIN, data_rate=data_rate)*0.125
             assert reading is not None
@@ -69,19 +68,38 @@ def test_readings_and_rate(readings_df):
     """Reads measurements for one minute."""
     import pandas as pd
     df = readings_df
-    assert len(df) == 9600
+    assert len(df) == 4800 # TODO Change this to an actual sensor count function
     all_adcs_readings = df['adc'].value_counts()
     for readings in all_adcs_readings:
         assert readings == 2400
     # TODO test rates based on df
 
+""" TODO
 def test_send_readings(readings_df):
-    import os 
+    import os
     import pandas as pd
-    from publisher.publisher import send_readings
-    
+    from publisher.publisher import send_readings, connect_to_broker
+
+    HOST = os.getenv("BROKER_IP", "mqtt.eclipse.org")
+    PORT = os.getenv("BROKER_PORT", 1883)
+    KEEPALIVE = 30
+    TOPIC = os.getenv("TOPIC", "usa/quincy/testing-pi")
+    GAIN = 1
+    DF_HEADERS = ['adc', 'channel', 'time_stamp', 'value']
+    client_id = "{0}".format("/TEN_HZ")
+    data_rate = 475
+
+    client, connection = connect_to_broker(
+        client_id=client_id,
+        host=HOST,
+        port=PORT,
+        keepalive=KEEPALIVE,
+        on_connect=None,
+        on_publish=None
+    )
+
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    send_readings(readings_df)
+    send_readings(readings_df, client)
     # test a csv file is created
     assert os.path.exists('{path}/{file}'.format(
         path=dir_path,
@@ -89,3 +107,5 @@ def test_send_readings(readings_df):
     ))
     # TODO: Test a CSV file is sent to the broker (with a fake broker container)
     pass
+"""
+
