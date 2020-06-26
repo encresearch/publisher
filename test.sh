62152377b4de3377a8539f945d4082815beb8de5
Sebastian Arboleda <seb.arboleda@outlook.com>
2020-06-26 16:21:50 -0400

    Fix Outdated Tests and Add pip-req files (#10)
    * Fix outdated tests
    
    * Add pip requirement files
    
    Co-authored-by: Seb <sebasarboleda22@gmail.com>

--- test.sh
+++ test.sh
@@ -5,9 +5,5 @@ printf "\n~~~ Running tests...this can take a few minutes ~~~\n"
 # Get project root
 PROJECT_ROOT=`pwd`
 
-# Activate conda environment
-printf "\nActivating the Conda virtual environment... \n"
-source activate publisher
-
 # Run tests with pytest
 python3 -m pytest $PROJECT_ROOT -s
